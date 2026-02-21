91a2d61fca2c60d5b0ee6674605691ea4d9ebe29
masoomshah787-ui <masoom.shah787@gmail.com>
2026-02-21 08:51:42 -0500

    Update pdf_chatbot.py

--- pdf_chatbot.py
+++ pdf_chatbot.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain_openai import ChatOpenAI
-from langchain.text_splitter import CharacterTextSplitter
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain.memory import ConversationBufferMemory
@@ -132,4 +131,5 @@ if st.session_state.processComplete:
 # Display initial instructions
 else:
     st.write("👈 Upload your PDFs in the sidebar to get started!")
+
 
